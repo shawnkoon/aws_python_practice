f1334f221917e8f801f8039abd19a5aedcf1d17f
shawnkoon <shawn9405@gmail.com>
2017-02-09 20:43:14 -0800

    Added practice code to read dynamo db tables via SDK

---
+++ dynamo_db/read_tables.py
@@ -0,0 +1,36 @@
+# Source : https://github.com/shawnkoon
+
+import boto3
+
+client = boto3.client('dynamodb')
+
+response = client.list_tables()
+
+print('\nPrinting response : type - ' + str(type(response)))
+print(str(response) + '\n')
+
+print('Printing each Table names : ')
+for table in response['TableNames']:
+	print(table)
+
+print('\n')
+print('Scanning values of each Tables : ')
+for table in response['TableNames']:
+	scan_result = client.scan(TableName=table)
+	print(scan_result)
+	
+	for item in scan_result['Items']:
+		print()
+		print(item)
+
+		for key, value in item.items():
+			print('key = ' + key + ', value = ' + str(value))
+	print()
+
+print('\n\nDone.')
+
+# ------ Things I learned -------
+# boto3.clinet('dynamodb') to access all dynamodb related functions.
+# Use items() to access all data in dict
+# DynamoDB is sorted with Partition key first, then Sort Key.
+# scan() returns all items related to that Table.
