ae5897bb1dc2cc2e71d41891f9cbf9af3be10352
shawnkoon <shawn9405@gmail.com>
2017-02-12 02:05:08 -0800

    Added waiter functionality to wait till the new dynamodb table is created

--- dynamo_db/create_table.py
+++ dynamo_db/create_table.py
@@ -1,8 +1,9 @@
 # Imports.
 import boto3
 
-# Get the service resource from AWS (dynamodb)
-dynamodb = boto3.resource('dynamodb')
+# Get the service client from AWS (dynamodb)
+dynamodb = boto3.client('dynamodb')
+# dynamodb = boto3.resource('dynamodb')
 
 tableName = 'exampleTable'
 
@@ -37,13 +38,20 @@ newTable = dynamodb.create_table(
 
 print('Creating table {0}....'.format(tableName))
 
+# Wait Until the finishes creating using waiter.
+waiter = dynamodb.get_waiter('table_exists')
+waiter.wait(
+	TableName=tableName,
+)
+
+print('Table {0} creation completed!\nBye.'.format(tableName))
 
 # This creates a table named users that respectively has the hash and range primary keys id and username along with first_name and last_name. 
 # This method will return a DynamoDB.Table resource to call additional methods on the created table.
 # src : https://boto3.readthedocs.io/en/latest/guide/dynamodb.html#creating-a-new-table
 
 # Things I found.
-# 1. boto3.resource && boto3.client are similar.
+# 1. boto3.resource && boto3.client are similar but client offers more features.
 # 2. Thre are 4 required attributes in create_table call.
 # 	a. TableName. - desired table name.
 #		b. AttributeDefinitions. - define each key schemas.
@@ -57,3 +65,7 @@ print('Creating table {0}....'.format(tableName))
 #			2. WriteCapacityUnits
 #		- https://boto3.readthedocs.io/en/latest/reference/services/dynamodb.html#DynamoDB.Client.create_table
 # 3. [] = list / {} = dict
+# 4. get_waiter only works with client.
+# 5. get_waiter has 2 different types.
+# 	a. 'table_exists'
+#		b. 'table_not_exists'
