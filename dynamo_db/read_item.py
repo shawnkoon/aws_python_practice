90f12c5867dba2ef618c92c0cb989db200f8e4c0
shawnkoon <shawn9405@gmail.com>
2017-02-22 22:03:48 -0800

    Added reading item from specific dynamodb table.

---
+++ dynamo_db/read_item.py
@@ -0,0 +1,22 @@
+# Import
+import boto3
+
+tableName = 'exampleTable'
+# Need to use resource to access Table obj
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(tableName)
+
+# Get one particular item from the table.
+print('About to fetch item with id = 1 from [{0}] table'.format(tableName))
+try:
+  response = table.get_item(
+    Key={
+      'id': 0,
+    },
+  )
+  item = response['Item']
+  print('Item with id 1 has username : [{0}]'.format(item['username']))
+except Exception as e:
+  print(e)
+  print('Please try again later.. might be an adding delay.')
+  
