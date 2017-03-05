089baa95f07fbe282265322d493e57491f3adae4
shawnkoon <shawn9405@gmail.com>
2017-03-04 22:59:35 -0800

    Added S3 bucket deletion code

---
+++ s3/delete_bucket.py
@@ -0,0 +1,28 @@
+# Import
+import boto3
+
+# Create S3 client.
+s3_client = boto3.client('s3')
+
+# Create bucket name desired to be deleted.
+bucket_name = 'shawnkoon-example-bucket'
+
+# Get all buckets.
+response = s3_client.list_buckets()
+
+# Iterate through buckets to see if `bucket_name` exists
+bucket_found = False
+print("Finding to see if {} exists.".format(bucket_name))
+for bucket in response['Buckets']:
+	if bucket['Name'] == bucket_name:
+		bucket_found = True
+
+# Bucket found? delete : exit
+if bucket_found:
+	print("Bucket {} found. Start Deleting...".format(bucket_name))
+	response = s3_client.delete_bucket(
+		Bucket=bucket_name
+	)
+	print("Bucket {} successfully deleted! :)".format(bucket_name))
+else:
+	print("Bucket {} NOT found.. Exiting the program.".format(bucket_name))
